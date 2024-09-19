c222f58b6b2f39b356b6e924def30dda4cec77e0
jaydenseto <jayden.seto11@gmail.com>
2024-09-19 09:34:11 -0400

    calc

---
+++ firstprogram.py
@@ -0,0 +1,11 @@
+# Basic calculator
+
+print("Welcome to calculator")
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+answer = num1 + num2
+print(f"Your answer is: {answer}!")
+
+
+
