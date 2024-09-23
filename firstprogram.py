1d260a7fbf5e3372575021e8418bc0c70a44c12b
jaydenseto <jayden.seto11@gmail.com>
2024-09-23 08:56:44 -0400

    calc

--- firstprogram.py
+++ firstprogram.py
@@ -4,8 +4,16 @@ print("Welcome to calculator")
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter another number: "))
 
-answer = num1 + num2
-print(f"Your answer is: {answer}!")
+choice = input("Do you want to: add, subtract, multiply, divide: ")
+
+if choice == "add":
+    print(num1 + num2)
+elif choice == "subtract":
+    print(num1 - num2)
+elif choice == "divide":
+    print(num1 / num2)
+elif choice == "multiply":
+    print(num1 * num2)
 
 
 
